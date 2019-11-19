29915767c1566315793816ab16d5ff5a7f62f672
ivan046f <48563214+ivan046f@users.noreply.github.com>
2019-11-20 01:08:34 +0500

    Player move and camera mover

---
+++ CameraMover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour
+{
+    private Transform playerTransform;
+
+    private void Awake()
+    {
+        playerTransform = Object.FindObjectOfType<Player>().transform;
+    }
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, -1);
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rigidBody;
+    private SpriteRenderer sprite;
+    private Animator animator;
+    private Vector2 input;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Rotation();
+        Animations();
+    }
+    private void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + input * speed * Time.fixedDeltaTime);
+    }
+    private void Rotation()
+    {
+        if (input.x > 0)
+            sprite.flipX = false;
+        if (input.x < 0)
+            sprite.flipX = true;
+    }
+    private void Animations()
+    {
+        if (input.x != 0 || input.y != 0)
+            animator.SetBool("IsRunning", true);
+        else
+            animator.SetBool("IsRunning", false);
+
+    }
+}
